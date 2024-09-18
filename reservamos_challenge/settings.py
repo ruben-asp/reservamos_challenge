2b9071a69a11b7aa03570bb65c8291c30291dbc0
ruben-asp <badgames@gmail.com>
2024-09-17 21:19:35 -0600

    Updating allowed hosts setting

--- reservamos_challenge/settings.py
+++ reservamos_challenge/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jb_v_eea4yv#439z_61ifjl()f-!y7x+3cu6ustl8dwh^z&=dk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
